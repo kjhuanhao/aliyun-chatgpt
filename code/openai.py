e5d14a843d6970192beac5acb9dbe60653fbbbce
赖佳豪 <2077780946@qq.com>
2022-12-16 10:57:26 +0800

    update

--- code/openai.py
+++ code/openai.py
@@ -1,10 +1,9 @@
 # -*- coding: utf-8 -*-
 # @Author   : LaiJiahao
-# @Time     : 2022/12/8 16:44
+# @Time     : 2022/12/16 10:29
 # @File     : openai.py
-# @Project  : openAI
-import random
-from config import configs
+# @Project  : aliyun-openAI
+
 import requests
 
 
@@ -13,12 +12,15 @@ class OpenAi:
         # 使用的模型:功能最强大的 GPT-3
         self.model = "text-davinci-003"
         self.url = "https://api.openai.com/v1/completions"
-
-    def get_answer(self,prompt,max_tokens,temperature):
-        keys = configs['keys']
-        key = random.choices(keys)
-        api_key = key[0]
-        if max_tokens <= 4096 and temperature <= 0.9:
+        # 替换为你的api_key <https://beta.openai.com/account/api-keys>
+        self.keys = "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"
+        # 这个是设置回答的长度,最大可以设置到4096 (免费额度为$18,该值影响你的用量)
+        self.max_tokens = 40
+        # 值越高意味着模型将承担更多风险。对于更具创造性的应用程序，请尝试 0.9,建议0.5-0.6
+        self.temperature = 0.5
+    def get_answer(self,prompt):
+        api_key = self.keys
+        if self.max_tokens <= 4096 and self.temperature <= 0.9:
             if prompt:
                 headers = {
                     "Content-Type": "application/json",
@@ -29,8 +31,8 @@ class OpenAi:
                 data = {
                     "model": "text-davinci-003",
                     "prompt": prompt,
-                    "max_tokens": max_tokens,
-                    "temperature": temperature,
+                    "max_tokens": self.max_tokens,
+                    "temperature": self.temperature,
                 }
                 # Make the API request
                 response = requests.post(self.url, headers=headers, json=data)
@@ -44,5 +46,5 @@ class OpenAi:
             answer = '你的max_tokens或temperature值过大！'
         return answer
 
-# ai = OpenAi(prompt='你好')
-# print(ai.get_answer())
+# ai = OpenAi()
+# print(ai.get_answer(prompt='你好'))
---
+++ readme.md
@@ -0,0 +1,53 @@
+# aliyun-chatgpt 帮助文档
+
+aliyun-chatpt是基于最近较火的chatgpt开发的一个项目，本项目代码十分简单，
+通过简单调用openai的接口来实现功能，有如下的优点和缺点：
+
+优点：
+1. 调用不会受到chatgpt官网的限制
+2. 个人使用大概只需要1个月更换一次key，且无需繁琐配置token
+3. 阿里云函数首年免费，后续如果继续基于本项目使用，收费一天也不超过1块钱
+4. 返回的字符数量和回答精度一定程度上可以自由设置
+
+缺点：
+1. 无法让AI继续，只能重新提问
+
+
+
+## 前期准备
+1. 使用该项目，请先开通阿里云`函数计算FC`
+2. 将本项目fork，且提前修改好`openai.py`内的如下设置
+```python
+def __init__(self):
+    # 替换为你的api_key <https://beta.openai.com/account/api-keys>
+    self.keys = "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"
+    # 这个是设置回答的长度,最大可以设置到4096 (免费额度为$18,该值影响你的用量)
+    self.max_tokens = 40
+    # 值越高意味着模型将承担更多风险。对于更具创造性的应用程序，请尝试 0.9,建议0.5-0.6
+    self.temperature = 0.5
+```
+
+# 代码 & 预览
+该项目基于: https://github.com/kjhuanhao/chatgpt-magic-plug
+修改而来，原项目是油猴插件，您可以先进入该项目地址查看是否符合你的预期
+
+
+## 部署 & 体验
+文字部署教程：
+
+视频部署教程：待制作
+
+
+
+
+
+## 交流
+- 如果有关于错误的反馈或者未来的期待，您可以通过以下几个方式联系
+  - 本项目的`issues`
+  - 作者邮箱：`mrlaijiahao@126.com`
+  - 作者的B站：`mrhuanhao`
+
+欢迎大家一起开源，优化这个基础项目
+
+### 已知的问题
+-[ ] 未做返回的代码优化
